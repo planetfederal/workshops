ef966ff23b2c6261ffcbc3ae7701dde7cb91b63b
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2014-06-21 19:35:37 +0200

    setup suite intro for internationalization IT

--- workshops/suiteintro/doc/source/conf.py
+++ workshops/suiteintro/doc/source/conf.py
@@ -247,3 +247,8 @@ latex_elements = {
 
 # If false, no module index is generated.
 latex_use_modindex = True
+
+# -- Options for internationalization --------------------------------------------------
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False	    # optional
+language = 'it'
