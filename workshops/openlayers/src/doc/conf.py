5b6dca86c9064f7156770fc041fdb797092f5cdb
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2014-07-04 11:31:49 +0200

    setup OpenLayer for internationalization IT

--- workshops/openlayers/src/doc/conf.py
+++ workshops/openlayers/src/doc/conf.py
@@ -247,3 +247,9 @@ latex_elements = {
 
 # If false, no module index is generated.
 latex_use_modindex = False
+
+# -- Options for internationalization --------------------------------------------------
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False	    # optional
+language = 'it'
+
