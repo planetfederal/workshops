57264d3bdfad3bfc9787752a302588fbf043410f
Paul Ramsey <pramsey@cleverelephant.ca>
2014-07-10 12:43:07 -0700

    Back out "upgrade" to latest ol3

--- tutorials/censusmap/static/code/censusmap.js
+++ tutorials/censusmap/static/code/censusmap.js
@@ -15,7 +15,7 @@ olMap = new ol.Map({
   target: 'map',
   renderer: ol.RendererHint.CANVAS,
   layers: [osmLayer, wmsLayer],
-  view: new ol.View({
+  view: new ol.View2D({
     center: [-10764594.0, 4523072.0],
     zoom: 5
   })
